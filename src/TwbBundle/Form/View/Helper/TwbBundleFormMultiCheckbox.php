15d12bdfe50492ecbd576a6a4f0d6f9340c4d2d2
Jean Rumeau <rumeau@gmail.com>
2013-10-14 14:21:05 -0300

    Fix syntax error

--- src/TwbBundle/Form/View/Helper/TwbBundleFormMultiCheckbox.php
+++ src/TwbBundle/Form/View/Helper/TwbBundleFormMultiCheckbox.php
@@ -16,8 +16,8 @@ class TwbBundleFormMultiCheckbox extends \Zend\Form\View\Helper\FormMultiCheckbo
 			 
 		if (empty($aLabelAttributes['class'])) $aLabelAttributes['class'] = $match;
 		elseif (!preg_match('/(\s|^)'.$match.'(\s|$)/',$aLabelAttributes['class'])) $aLabelAttributes['class'] .= ' '.$match;
-			$oElement->setLabelAttributes($aLabelAttributes);
-		}
+		$oElement->setLabelAttributes($aLabelAttributes);
+		
 		return parent::render($oElement);
 	}
 }
