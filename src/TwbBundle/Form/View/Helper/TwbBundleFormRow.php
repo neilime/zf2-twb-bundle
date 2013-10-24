179ca7e38143742a5e3252e9e8f3100ad19e773f
Tomdarkness <tomwpryor@gmail.com>
2013-10-24 16:48:25 +0100

    Render help block in right place on horizontal form

--- src/TwbBundle/Form/View/Helper/TwbBundleFormRow.php
+++ src/TwbBundle/Form/View/Helper/TwbBundleFormRow.php
@@ -54,7 +54,9 @@ class TwbBundleFormRow extends \Zend\Form\View\Helper\FormRow{
 		//Render errors
 		$this->renderErrors($oElement).
 		//Render help block
-		$this->renderHelpBlock($oElement);
+		if($sLayout !== \TwbBundle\Form\View\Helper\TwbBundleForm::LAYOUT_HORIZONTAL) {
+			$this->renderHelpBlock($oElement);
+		}
 
 		//Render form row
 		$sElementType = $oElement->getAttribute('type');
@@ -149,7 +151,7 @@ class TwbBundleFormRow extends \Zend\Form\View\Helper\FormRow{
 						self::$horizontalLayoutFormat,
 						$sLabelOpen.$sLabelContent.$sLabelClose,
 						$sClass,
-						$this->getElementHelper()->render($oElement)
+						$this->getElementHelper()->render($oElement) . $this->renderHelpBlock($oElement)
 					);
 				default:
 					throw new \DomainException('Layout "'.$sLayout.'" is not valid');
