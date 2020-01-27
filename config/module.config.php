873d6421c0db4b22d24a13e28ddbb9b4d65345df
Emilien Escalle <neilime@users.noreply.github.com>
2020-01-27 22:55:07 +0100

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -102,14 +102,12 @@ return array(
             'form_element' => 'TwbBundle\Form\View\Helper\Factory\TwbBundleFormElementFactory',
             'formelement' => 'TwbBundle\Form\View\Helper\Factory\TwbBundleFormElementFactory',
             'TwbBundle\Form\View\Helper\TwbBundleFormElement' => 'TwbBundle\Form\View\Helper\Factory\TwbBundleFormElementFactory',
-
-            'navigation'                => 'TwbBundle\Navigation\View\NavigationHelperFactory',
             'zendviewhelpernavigation'  => 'TwbBundle\Navigation\View\NavigationHelperFactory',
         ),
         'aliases' => array (
             'form_element' => 'TwbBundle\Form\View\Helper\TwbBundleFormElement',
-
-            'Navigation'   => 'TwbBundle\Navigation\View\NavigationHelperFactory',
+            'navigation' => 'zendviewhelpernavigation',
+            'Navigation' => 'zendviewhelpernavigation',
         ),
     ),
 );
